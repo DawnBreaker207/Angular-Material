a6aa795ad5f80c04cd75d0ea5f48f114bd0910c7
DawnBreaker <tunganhngo207@gmail.com>
2024-12-25 17:27:00 +0700

    feat: fix bug

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatIcon } from '@angular/material/icon';
 import { MatMiniFabButton } from '@angular/material/button';
-import { map, Observable } from 'rxjs';
+import { EMPTY, map, Observable } from 'rxjs';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
@@ -16,8 +16,8 @@ import { MatTab, MatTabGroup } from '@angular/material/tabs';
 })
 export class HomeComponent implements OnInit {
   coursesService = inject(CoursesService);
-  begginerCourses$: Observable<Course[]> = [];
-  advancedCourses$: Observable<Course[]> = [];
+  begginerCourses$: Observable<Course[]> = EMPTY;
+  advancedCourses$: Observable<Course[]> = EMPTY;
 
   ngOnInit() {
     const courses$ = this.coursesService.findAllCourses();
