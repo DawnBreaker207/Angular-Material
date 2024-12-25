17439f795c99bd9fd75fc12d9d8449d68792e73b
DawnBreaker <tunganhngo207@gmail.com>
2024-12-25 15:33:18 +0700

    - First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatSidenavContainer} from "@angular/material/sidenav";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {MatListItem, MatNavList} from "@angular/material/list";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavContainer, MatToolbar, MatIconButton, MatIcon, MatNavList, MatListItem, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {CourseComponent} from "./course/course.component";
+import {CreateCourseComponent} from "./create-course/create-course.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "about", component: AboutComponent},
+  {path: "courses/:id", component: CourseComponent},
+  {
+    path: 'add-new-course', component: CreateCourseComponent
+  }, {
+    path: "**", redirectTo: "/"
+  }
+];
